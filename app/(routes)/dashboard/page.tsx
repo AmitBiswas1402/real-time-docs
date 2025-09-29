e22d644366ecb84380f16a41d67dcc0f0b24560d
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-09-29 23:53:52 +0530

    feat: add Convex client provider and integrate with layout
    - Created ConvexClientProvider component to wrap application with ConvexProvider.
    - Updated RootLayout to include ConvexClientProvider for state management.
    - Added README.md for Convex functions directory with usage examples for queries and mutations.
    - Generated API and server utilities for Convex functions.
    - Implemented user query function to retrieve user by email.
    - Updated package.json and package-lock.json to include Convex dependency.

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,6 +1,25 @@
-const page = () => {
+import { api } from "@/convex/_generated/api";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useQuery } from "convex/react";
+import { useEffect } from "react";
+
+function Dashboard() {
+  const { user } = useKindeBrowserClient();
+  const getUser = useQuery(
+    api.user.getUser,
+    user?.email ? { email: user.email } : "skip"
+  );
+
+  // const createUser=useMutation(api.user.createUser);
+  useEffect(()=>{
+      
+  },[user])
+
   return (
-    <div>page</div>
+    <div>
+      Dashboard
+    </div>
   )
 }
-export default page
+
+export default Dashboard;
