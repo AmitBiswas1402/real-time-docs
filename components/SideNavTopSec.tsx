7448203fef66dbfbf8bbc7099059367f84c2667d
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-10-03 22:08:50 +0300

    feat: add logout functionality and separator component to SideNavTopSec

--- components/SideNavTopSec.tsx
+++ components/SideNavTopSec.tsx
@@ -1,26 +1,28 @@
-import { ChevronDown, Settings, Users } from "lucide-react";
+import { ChevronDown, LogOut, Settings, Users } from "lucide-react";
 import Image from "next/image";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import { Separator } from "./ui/separator";
 
 const SideNavTopSec = () => {
-  const menu=[
-        {
-            id:1,
-            name:'Create Team',
-            path:'/teams/create',
-            icon:Users
-        },
-        {
-            id:2,
-            name:'Settings',
-            path:'',
-            icon:Settings
-        }
-    ];
+  const menu = [
+    {
+      id: 1,
+      name: "Create Team",
+      path: "/teams/create",
+      icon: Users,
+    },
+    {
+      id: 2,
+      name: "Settings",
+      path: "",
+      icon: Settings,
+    },
+  ];
   return (
     <Popover>
       <PopoverTrigger>
@@ -36,6 +38,25 @@ const SideNavTopSec = () => {
         <div>
           <h2>Team Name</h2>
         </div>
+        <Separator className="mt-2 bg-slate-100" />
+        <div>
+          {menu.map((item, index) => (
+            <h2
+              key={index}
+              className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer"
+            >
+              <item.icon className="h-4 w-4" />
+              {item.name}
+            </h2>
+          ))}
+          <LogoutLink>
+            <h2 className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer">
+              <LogOut className="h-4 w-4" />
+              Logout
+            </h2>
+          </LogoutLink>
+        </div>
+        <Separator className="mt-2 bg-slate-100" />
       </PopoverContent>
     </Popover>
   );
