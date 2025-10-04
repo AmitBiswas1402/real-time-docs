f1d5e71e733f0dcdd4667b013e8d6a351d950b71
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-10-04 20:15:12 +0530

    feat: implement file creation and management features in SideNavbar and SideNavBottomSec components

--- components/SideNavTopSec.tsx
+++ components/SideNavTopSec.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, LogOut, Settings, Users } from "lucide-react";
+import { ChevronDown, LayoutGrid, LogOut, Settings, Users } from "lucide-react";
 import Image from "next/image";
 import {
   Popover,
@@ -7,8 +7,25 @@ import {
 } from "@/components/ui/popover";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import { Separator } from "./ui/separator";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
-const SideNavTopSec = () => {
+// interface UserProps {
+//   user: any;
+//   users?: any;
+//   setActiveTeamInfo: any;
+// }
+
+export interface TEAM {
+  createdBy: String;
+  teamName: String;
+  _id: String;
+}
+
+const SideNavTopSec = ({ user, setActiveTeamInfo }: any) => {
   const menu = [
     {
       id: 1,
@@ -23,42 +40,101 @@ const SideNavTopSec = () => {
       icon: Settings,
     },
   ];
+
+  const router = useRouter();
+  const convex = useConvex();
+  const [activeTeam, setActiveTeam] = useState<TEAM>();
+  const [teamList, setTeamList] = useState<TEAM[]>([]);
+
+  useEffect(() => {
+    user && getTeamList();
+  }, [user]);
+
+  useEffect(()=> {
+    activeTeam && setActiveTeamInfo(activeTeam)
+  }, [activeTeam])
+  
+  const getTeamList = async () => {
+    const result = await convex.query(api.teams.getTeam, {
+      email: user?.email,
+    });
+    console.log("TeamList: ", result);
+    setTeamList(result);
+    setActiveTeam(result[0]);
+  };
+
+  const onMenuClick = (item: any) => {
+    if (item.path) {
+      router.push(item.path);
+    }
+  };
   return (
-    <Popover>
-      <PopoverTrigger>
-        <div className="flex items-center gap-3 hover:bg-slate-200 p-2 rounded-md cursor-pointer">
-          <Image src="/logo-1.png" alt="" width={40} height={40} />
-          <h2 className="flex gap-2 items-center font-bold text-[17px]">
-            Team Name
-            <ChevronDown />
-          </h2>
-        </div>
-      </PopoverTrigger>
-      <PopoverContent className="ml-7 p-4">
-        <div>
-          <h2>Team Name</h2>
-        </div>
-        <Separator className="mt-2 bg-slate-100" />
-        <div>
-          {menu.map((item, index) => (
-            <h2
-              key={index}
-              className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer"
-            >
-              <item.icon className="h-4 w-4" />
-              {item.name}
+    <div>
+      <Popover>
+        <PopoverTrigger>
+          <div className="flex items-center gap-3 hover:bg-slate-200 p-2 rounded-md cursor-pointer">
+            <Image src="/logo-1.png" alt="" width={40} height={40} />
+            <h2 className="flex gap-2 items-center font-bold text-[17px]">
+              {activeTeam?.teamName}
+              <ChevronDown />
             </h2>
-          ))}
-          <LogoutLink>
-            <h2 className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer">
-              <LogOut className="h-4 w-4" />
-              Logout
-            </h2>
-          </LogoutLink>
-        </div>
-        <Separator className="mt-2 bg-slate-100" />
-      </PopoverContent>
-    </Popover>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="ml-7 p-4">
+          <div>
+            {teamList?.map((team, index) => (
+              <h2
+                key={index}
+                className={`p-2 hover:bg-blue-500 hover:text-white rounded-lg mb-1 cursor-pointer ${activeTeam?._id == team._id && "bg-blue-500 text-white"}`}
+                onClick={() => setActiveTeam(team)}
+              >
+                {team.teamName}
+              </h2>
+            ))}
+          </div>
+          <Separator className="mt-2 bg-slate-100" />
+          <div>
+            {menu.map((item, index) => (
+              <h2
+                key={index}
+                className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer"
+                onClick={() => onMenuClick(item)}
+              >
+                <item.icon className="h-4 w-4" />
+                {item.name}
+              </h2>
+            ))}
+            <LogoutLink>
+              <h2 className="flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer">
+                <LogOut className="h-4 w-4" />
+                Logout
+              </h2>
+            </LogoutLink>
+          </div>
+          <Separator className="mt-2 bg-slate-100" />
+          {user && (
+            <div className="mt-2 flex gap-2 items-center">
+              <Image
+                src={user?.picture}
+                alt="user"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              <div>
+                <h2 className="text-[14px] font-bold">{user?.given_name}</h2>
+                <h2 className="text-[12px] font-gray-500">{user?.email}</h2>
+              </div>
+            </div>
+          )}
+        </PopoverContent>
+      </Popover>
+
+      {/* All file button */}
+      <Button variant="outline" className="w-full justify-start gap-2 font-bold mt-8 bg-gray-100 cursor-pointer">
+        <LayoutGrid className="h-5 w-5" /> All Files
+      </Button>
+    </div>
   );
 };
 export default SideNavTopSec;
